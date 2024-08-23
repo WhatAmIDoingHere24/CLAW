c2fe9cf93aa0d79cdd20a7b7c35911a5ba35ce11
WhatAmIDoingHere24 <106035602+WhatAmIDoingHere24@users.noreply.github.com>
2024-08-23 15:35:14 -0700

    Create CLAW.py

---
+++ CLAW.py
@@ -0,0 +1,33 @@
+#claw ideas/plan
+#Things that AXE needs to be able to do:
+  #be able to encrypt/decrypt
+  #pull metadata from images
+  #find hidden encryptioon in images
+  #hold all the cyberpatriots scripts
+  #be able to "easy snap" script's like lego bricks
+  #find files (think foresnics queston "find hiddden mp3 file")
+  #add pytorch to guess encyptions/also allows us to better crack encryption
+
+#GUI ideas
+  #CustomTKinter GUI
+  #Pillow (PIL) for adding images to GUI
+  #be able to download output
+  #
+  #
+  #
+  #
+  #
+
+#! W I P !
+#a menue yo select common searchable items on linux and return a value (like id -u but with a menue and take prompt)
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
