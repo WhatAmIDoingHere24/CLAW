e3722ecab16cc1266d676ab0aa8190eef546eb6e
Adalace1 <165712941+JoshuaSizemore1@users.noreply.github.com>
2024-09-01 12:09:01 -0600

    9-1-2024_Button_Name_Fix

--- command.py
+++ command.py
@@ -2,7 +2,7 @@ from crypto import encodeBase64,decodeBase64
 from windowScriptManager import scriptManager
 
 #List of tools to be assinged to buttons
-toolList = ["encode Base64","decode Base64", "window Scripts", "C.I.A (Cipher Index Analysis)"]
+toolList = ["encodeBase64","decodeBase64", "windowScripts", "C.I.A (Cipher Index Analysis)"]
 
 def command(commandName):
     match commandName:
