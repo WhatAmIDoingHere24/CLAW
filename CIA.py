2c1593fa5f12ac29fcad3a680f86bd976a0f9f7a
WhatAmIDoingHere24 <106035602+WhatAmIDoingHere24@users.noreply.github.com>
2024-09-02 07:36:07 -0700

    linux stuff

--- CIA.py
+++ CIA.py
@@ -2,8 +2,34 @@ from functools import partial
 from crypto import  encodeBase64, decodeBase64, encodeBase32, decodeBase32
 
 def cia():
-    print("hello world")
-    
+    #Sets default apperence to your systems default, and sets color theme to blue
+    customtkinter.set_appearance_mode("System")
+    customtkinter.set_default_color_theme("blue")
+
+    #Initilizes the application screen 
+    toolWindow = customtkinter.CTk()
+    toolWindow.title("encodeBase64")
+    toolWindow.geometry('300x300')
+    toolWindow.resizable(False, False)
+
+
+    #Initilizes text box
+    entry = customtkinter.CTkEntry(toolWindow, placeholder_text= "Enter text", placeholder_text_color= "grey", width=150, 
+                                   height=50, text_color="black", fg_color= "light grey", border_width= 0, corner_radius= 5)
+    #Initilizes result label
+    resultLabel = customtkinter.CTkTextbox(toolWindow, width=150, height=50, text_color="black")
+    resultLabel.configure(state= "disabled")
+
+    #Adds all widgets to screen
+    resultLabel.pack(anchor= "nw", padx = 10, pady = 10, fill= customtkinter.BOTH, expand= True)
+    entry.pack(anchor= "nw", padx = 10, pady = 10, fill= customtkinter.X, side= customtkinter.BOTTOM)
+
+
+    #Handels enter button being pressed to submit entry text to conversion
+    #toolWindow.bind('<Return>', lambda event: convert2base64(event, entry.get()))
+    toolWindow.bind('<Return>', handleEntryEnter)
+    entry.focus_force()
+    toolWindow.mainloop()
 
 """
 Cipher index (a summation of a bunch of indicators for pattern recong. / repition / identiiers for crypto)
