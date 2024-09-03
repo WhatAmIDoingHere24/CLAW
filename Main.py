58a0be70dcf834c0ec3a925fea959878c464666a
JoshuaSizemore1 <joshjsizemore@gmail.com>
2024-09-02 20:58:25 -0600

    9-2-2024_Rework_Base_GUI

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 import customtkinter
-from command import command, toolList, titleList
+from command import command, titleCardCommand, toolList, titleList
 from functools import partial
 
 """
@@ -24,28 +24,38 @@ claw.title("CLAW")
 
 
 #Creates label (1 for now)
-title_bar_frame = customtkinter.CTkScrollableFrame(claw, height=100, fg_color= "transparent",corner_radius= 0, border_width= 0)
+title_bar_frame = customtkinter.CTkScrollableFrame(claw, height= 90, fg_color= "transparent",corner_radius= 0, border_width= 0)
 title_bar_frame._scrollbar.configure(height=0)
 
-
-#Ceates the 3 colums that the different buttons are put into
+rectLabel = customtkinter.CTkLabel(claw, text= "", fg_color= "light grey", corner_radius= 10, height= 2)
+#Ceates the 3 buttonColums that the different buttons are put into
 columFrame= customtkinter.CTkScrollableFrame(claw, fg_color= "transparent", border_width= 0, corner_radius= 0)
 
-colums = []
+buttonColums = []
 for i in range(3):
-    colum_name = "colum" + str(i)
-    colums.append(exec("%s = None" % (colum_name)))
-    colums[i] = customtkinter.CTkFrame(columFrame, width= ((claw_size[0] / 3) -10), fg_color= "transparent", 
+    colum_name = "buttonColum" + str(i)
+    buttonColums.append(exec("%s = None" % (colum_name)))
+    buttonColums[i] = customtkinter.CTkFrame(columFrame, width= ((claw_size[0] / 3) -10), fg_color= "transparent", 
                                 border_width= 0, corner_radius= 0)
+    
+titleColum = []
+for i in range(4):
+    colum_name = "titleColum" + str(i)
+    titleColum.append(exec("%s = None" % (colum_name)))
+    titleColum[i] = customtkinter.CTkFrame(title_bar_frame, height= 90, width= ((claw_size[0] / 4) -10), 
+                                             fg_color= "transparent", border_width= 0, corner_radius= 0)
 
 
 #Adds the title_bar and all 3 colum widgets onto the screen
 #title_bar.pack(anchor= "nw", padx= 5, pady = 5)
 title_bar_frame.pack(anchor= "nw", fill= customtkinter.X)
-columFrame.pack(anchor= "nw", fill = customtkinter.BOTH, expand= True)
-for colum in colums:
-    #Setting pack_propagate to False makes the frames not expand to hold the widgets in it
+rectLabel.pack(anchor= "nw", padx= 10, pady= 5, fill= customtkinter.X)
+columFrame.pack(anchor= "nw", fill = customtkinter.BOTH, expand= True, side= customtkinter.TOP)
+for colum in buttonColums:
     colum.pack(anchor= "nw", fill= customtkinter.Y, side = customtkinter.LEFT, expand= True)
+
+for colum in titleColum:
+    colum.pack(anchor= "nw", pady= 5, fill= customtkinter.Y, side = customtkinter.LEFT, expand= True)
 
 #Holds all the tool Buttons from toolList 
 toolButtons = []
@@ -53,12 +63,12 @@ titleCards = []
 invalidChar = [" ", ".", "(", ")", "/"]
 
 #Iterates through toolList to create buttons with pairing function from command file
-counter = 0 #Counter used to start new row of buttons once all four colums get one button
 for i in range(len(toolList)):
     toolButtons.append([])
+    counter = 0 #Counter used to start new row of buttons once all three buttonColums get one button
     for j in range(len(toolList[i])):
         #Creates a function with the argument included (you cant pass an argument into a button command without this)
-        newCommand = partial(command,toolList[i][j], None, None, None)
+        newCommand = partial(command,toolList[i][j])
         #Creates button with new name and command
 
         button_name = toolList[i][j]
@@ -79,7 +89,7 @@ for i in range(len(toolList)):
 
 
         toolButtons[i].append(exec("%s = None" % (button_name)))
-        toolButtons[i][j] = customtkinter.CTkButton(master= colums[counter], text=button_text, height= 80, 
+        toolButtons[i][j] = customtkinter.CTkButton(master= buttonColums[counter], text=button_text, height= 80, 
                                                 width= ((claw_size[0] / 3) - 30),fg_color="blue", command=newCommand)
 
         if(counter % 2 == 0) and (counter > 0):
@@ -87,13 +97,19 @@ for i in range(len(toolList)):
         else:
             counter = counter + 1
 
+
+
 #Iterates through TitleList to create a radio button for each title with a function from command
-radioVar = customtkinter.StringVar()
-radioVar.set(None)
+counter = 0
 for i in range(len(titleList)):
 
+    titleText = titleList[i]
     titleName= titleList[i]
-    newCommand = partial(command,titleList[i], toolButtons, i, colums)
+    newCommand = partial(titleCardCommand, titleList[i], toolButtons, i, titleCards)
+
+    for n in range(len(titleText)):
+        if titleText[n] == " ":
+            titleText = titleText[:n] + "\n" + titleText[n+1:]
 
     for j in range(len(titleName)):
         for charcter in invalidChar:
@@ -101,9 +117,15 @@ for i in range(len(titleList)):
                 titleName = titleName[:j] + "_" + titleName[j+1:]
 
     titleCards.append(exec("%s = None" % (titleName)))
-    titleCards[i] = customtkinter.CTkButton(title_bar_frame, text= titleList[i], height= 60, width=40, 
-                                            fg_color="blue",  command=newCommand)
-    titleCards[i].pack(padx = 20, pady = 5, side= customtkinter.LEFT)
-    pass
+    titleCards[i] = customtkinter.CTkButton(titleColum[counter], text= titleText, height= 60, width=((claw_size[0] / 4) - 50), 
+                                            fg_color="dark grey", hover= False, command=newCommand)
+    titleCards[i].pack_propagate(False)
+    titleCards[i].pack(padx = 20, pady = 10)
+    if i == 0:
+        newCommand()
+    if(counter % 3 == 0) and (counter > 0):
+        counter = 0
+    else:
+        counter = counter + 1
 #runs the window until it is closed by user
 claw.mainloop()
