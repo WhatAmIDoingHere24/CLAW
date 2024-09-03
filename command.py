6150498cd2b884d3d859238a6c08383b2f73779f
JoshuaSizemore1 <joshjsizemore@gmail.com>
2024-09-02 20:58:25 -0600

    9-2-2024_Rework_Base_GUI

--- command.py
+++ command.py
@@ -6,35 +6,19 @@ from CIA import cia
 from CodeRed import CDRD
 
 #List of tools to be assinged to buttons
-titleList = ["Encryption / Decrption", "Window Scripts", "Linux Scripts", "CTF Tools"]
 cryptoToolList = ["encodeBase64","decodeBase64", "encodeBase32", "decodeBase32", "Cipher Index Analysis", "Cipher Recognition","Shift Cipher"]
 winScriptToolList = ["windowScripts"]
 linScriptToolList = ["linuxScripts"]
-ctfToolList = ["codeRed","osint","metaDataGrabber","www"]
+ctfToolList = ["codeRed", "osint", "metaDataGrabber", "www"]
 
+#Must match index of titleList and toolList if you want said title to show said tool buttons
+titleList = ["Encryption / Decrption", "Window Scripts", "Linux Scripts", "CTF Tools"]
 toolList = [cryptoToolList, winScriptToolList, linScriptToolList,ctfToolList]
 
-def packButtons(buttonList, index, colums):
-    counter = 0
-    for i in range(len(buttonList)):
-        if i == index:
-            for j in range(len(buttonList[i])):
-                #buttonList[i][j].configure(master= colums[counter])
-                buttonList[i][j].pack(padx = 10, pady = 15, side= customtkinter.TOP)
-        else:
-            for n in range(len(buttonList[i])):
-                buttonList[i][n].pack_forget()
+        
 
-def command(commandName, toolList, index, colums):
+def command(commandName):
     match commandName:
-        case "Encryption / Decrption":
-            packButtons(toolList, index, colums)
-        case "Window Scripts":
-            packButtons(toolList, index, colums)
-        case "Linux Scripts":
-            packButtons(toolList, index, colums)
-        case "CTF Tools":
-            packButtons(toolList, index, colums)
         case "encodeBase64":
             print("this is base64")
             encodeBase64()
@@ -56,5 +40,29 @@ def command(commandName, toolList, index, colums):
             CDRD()
         
     
-            
+def packButtons(buttonList, index, titleList):
+    for i in range(len(titleList)):
+        if i == index:
+            titleList[i].configure(fg_color=  "grey")
+        else:
+            titleList[i].configure(fg_color=  "dark grey")
+    for i in range(len(buttonList)):
+        if i == index:
+            for j in range(len(buttonList[i])):
+                buttonList[i][j].pack(padx = 10, pady = 15, side= customtkinter.TOP)
+        else:
+            for n in range(len(buttonList[i])):
+                buttonList[i][n].pack_forget()
+
+
+def titleCardCommand(commandName, toolList, index, titleList):
+    match commandName:
+        case "Encryption / Decrption":
+            packButtons(toolList, index, titleList)
+        case "Window Scripts":
+            packButtons(toolList, index, titleList)
+        case "Linux Scripts":
+            packButtons(toolList, index, titleList)
+        case "CTF Tools":
+            packButtons(toolList, index, titleList)       
 
