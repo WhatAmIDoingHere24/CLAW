4e7e57756406a527965e0adf16ad92c8802d368a
WhatAmIDoingHere24 <106035602+WhatAmIDoingHere24@users.noreply.github.com>
2024-09-03 09:37:43 -0700

    Update CipherID.py

--- CipherID.py
+++ CipherID.py
@@ -1,29 +1,3 @@
-#this is the main resean we devolped CLAW right here. it is 
-#the ciper identification tool that make this whole project worth it
-
-import torch
-from CIA import cia
-from crypto import encodeBase64, decodeBase64, encodeBase32, decodeBase32
-
-"""
-Jett's plans on how pytorch will work
-
-Pre-Requisets
-    -a seet of encrypted text paitred with matching unencrypted text
-    -more CIA tools
-
-1.user will enter the encrypted string into pytorch
-2.pytorch will than do a few things at once
-    -run that string char by char agiunast alll other strings to try and find a few tghat masy mathc
-    -if a rough match is found run more test going char by char to de4duce how similar they are to erach other
-3.pyutorch than runs the given encryptiont brough CIA and looks at what stats it gets
-4.pytorch than matches those stats agoianst all other encryptions that may have those same stats
-5.once a suitible match is found try the most proballble decryption algorythem
-6. once done return what encryptio it is and tyhe given string
-
-"""
-
-
-def torchTest():
-    print("it works")
-
+#this fule will now be used as our main interface with ciphey 
+#ciphey is a very quick and isefull tool for cioher identification and 
+#decryption
