a9897d420a3c141f98b55dd1998f70f26582d3a0
WhatAmIDoingHere24 <106035602+WhatAmIDoingHere24@users.noreply.github.com>
2024-09-04 06:31:27 -0700

    it all works fine on my machine no bugs here

--- ciphy.py
+++ ciphy.py
@@ -1,3 +1,19 @@
+#theres two ways we can use ciphey
+#either ass a library which is kinda weird
+#or as CLI which is much easier
+"""
+import ciphey
+from ciphey import decrypt
+from ciphey.iface import Config
+
+res = decrypt(
+        Config().library_default().complete_config(),
+        "SGVsbG8gbXkgbmFtZSBpcyBiZWUgYW5kIEkgbGlrZSBkb2cgYW5kIGFwcGxlIGFuZCB0cmVl",
+    )
+
+"""
+    
+"""
 import sys
 import os
 import subprocess
@@ -8,3 +24,5 @@ x = "python3 -m pip install ciphey --upgrade"
 
 def homeDir():
     subprocess.run(["powershell", x], shell=True)
+
+""""
