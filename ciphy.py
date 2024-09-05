40e1b7bf791a653d47f4807b2a9979526b510b78
WhatAmIDoingHere24 <106035602+WhatAmIDoingHere24@users.noreply.github.com>
2024-09-05 06:32:21 -0700

    yes

--- ciphy.py
+++ ciphy.py
@@ -1,17 +1,13 @@
 #theres two ways we can use ciphey
 #either ass a library which is kinda weird
 #or as CLI which is much easier
-"""
+
 import ciphey
 from ciphey import decrypt
 from ciphey.iface import Config
 
-res = decrypt(
-        Config().library_default().complete_config(),
-        "SGVsbG8gbXkgbmFtZSBpcyBiZWUgYW5kIEkgbGlrZSBkb2cgYW5kIGFwcGxlIGFuZCB0cmVl",
-    )
 
-"""
+
     
 """
 import sys
