67b362b887ced770f7291f099247d80b283d2057
WhatAmIDoingHere24 <106035602+WhatAmIDoingHere24@users.noreply.github.com>
2024-09-05 06:34:29 -0700

    having troubles with the command file?? idk

--- ciphy.py
+++ ciphy.py
@@ -1,12 +1,11 @@
 #theres two ways we can use ciphey
 #either ass a library which is kinda weird
 #or as CLI which is much easier
-
+"""
 import ciphey
 from ciphey import decrypt
 from ciphey.iface import Config
-
-
+"""
 
     
 """
@@ -21,4 +20,4 @@ x = "python3 -m pip install ciphey --upgrade"
 def homeDir():
     subprocess.run(["powershell", x], shell=True)
 
-""""
+"""
